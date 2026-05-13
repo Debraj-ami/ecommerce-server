70c7f41747ef2d56f0b1c3cfef834394d4099a83
Debraj Mondal <debrajmondal@gmail.com>
2026-05-13 21:32:49 +0530

    fixed production category filtering

--- src/main/java/com/debraj/repository/ProductRepository.java
+++ src/main/java/com/debraj/repository/ProductRepository.java
@@ -20,10 +20,19 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     // ✅ PAGINATION + FILTER QUERY (FINAL)
     @Query("""
     	    SELECT p FROM Product p
-    	    WHERE (:category IS NULL OR :category = '' OR p.category.name = :category)
-    	    AND ((:minPrice IS NULL AND :maxPrice IS NULL)
-    	         OR (p.discountedPrice BETWEEN :minPrice AND :maxPrice))
-    	    AND (:minDiscount IS NULL OR p.discountPercent >= :minDiscount)
+    	    WHERE (
+    	        :category IS NULL 
+    	        OR :category = '' 
+    	        OR LOWER(p.category.name) = LOWER(:category)
+    	    )
+    	    AND (
+    	        (:minPrice IS NULL AND :maxPrice IS NULL)
+    	        OR (p.discountedPrice BETWEEN :minPrice AND :maxPrice)
+    	    )
+    	    AND (
+    	        :minDiscount IS NULL 
+    	        OR p.discountPercent >= :minDiscount
+    	    )
     	""")
     	Page<Product> filterProducts(
     	        @Param("category") String category,
